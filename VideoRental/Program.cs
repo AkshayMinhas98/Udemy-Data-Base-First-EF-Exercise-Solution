ce7fecb75b6f2dd90fa5b6e30bea1a76d220766b
Akshay Minhas <Akshay.Minhas@rsystems.com>
2018-10-24 18:44:02 +0530

    Add project files.

---
+++ VideoRental/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoRental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new VidzyContextEntities();
+            //var data = new Video()
+            //{
+            //    Id = 1,
+            //    Name = "ASA",
+            //    ReleaseDate = DateTime.Now
+            // };
+            DateTime x = DateTime.Now;
+            var y = Convert.ToByte(Classification.Platinum);
+            //context.Videos.Add(data);
+            // context.SaveChanges();
+            context.AddVideo("Maze",x, "Horror",y);
+        }
+        
+    }
+}
